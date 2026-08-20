013af4506eb2ceaf15d46424bed8d0e23618c15a
geomoded-sdk <geomoded.sdk@gmail.com>
2026-08-20 15:57:12 -0300

    fix: debian-installer false (none não é suportado no live-build 3.0~a57)

--- Scripts/build-iso.sh
+++ Scripts/build-iso.sh
@@ -61,7 +61,7 @@ lb config \
   --initsystem systemd \
   --binary-images iso-hybrid \
   --bootappend-live "$BOOTARGS" \
-  --debian-installer none \
+  --debian-installer false \
   --debian-installer-distribution "$SUITE" \
   --apt-recommends false \
   --security false \
